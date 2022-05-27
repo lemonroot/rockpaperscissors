e2d80d5c6be616bb47b6391132943548d9637894
lemonroot <79768526+lemonroot@users.noreply.github.com>
2022-05-27 11:38:44 -0400

    Input reading
    Player's input is read and compared to computer's input.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-computerPlay();
+let answer = prompt("Please input Rock, Paper, or Scissors.");
+
+playGame(answer);
 
 function computerPlay(){
     let moves = ['Rock', 'Paper', 'Scissors'];
@@ -6,4 +8,11 @@ function computerPlay(){
     let moveChoice = Math.floor(Math.random() * moves.length);
     console.log(moves[moveChoice]);
     return moves[moveChoice];
+}
+
+function playGame(answer){
+    console.log(answer);
+    if(answer.toUpperCase == computerPlay().toUpperCase){
+        console.log("same")
+    } else{console.log("different")}
 }
